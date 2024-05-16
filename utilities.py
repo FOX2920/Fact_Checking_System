3ecc32545335ad0c8018a7114cf2649ad97ff2a8
TRAN THANH SON <91709267+FOX2920@users.noreply.github.com>
2024-05-16 11:53:46 +0700

    Update utilities.py

--- utilities.py
+++ utilities.py
@@ -4,26 +4,42 @@ import logging
 logging.disable(logging.WARNING)
 
 import torch
+import numpy as np
+import random
 from transformers import AutoModel, AutoTokenizer
 from Mbert import MBERTClassifier
 # from evidence_retrieval import evidence_top_n, similarities
 #import gc
 from config import hf_token
 
-#device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+# Thiết lập seed cố định
+def set_seed(seed):
+    random.seed(seed)
+    np.random.seed(seed)
+    torch.manual_seed(seed)
+    if torch.cuda.is_available():
+        torch.cuda.manual_seed(seed)
+        torch.cuda.manual_seed_all(seed)
+    torch.backends.cudnn.deterministic = True
+    torch.backends.cudnn.benchmark = False
+
+# Gọi hàm set_seed với seed cố định, ví dụ: 42
+set_seed(42)
+
+# device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 device = torch.device("cpu")
 modelname = "SonFox2920/MBert_FC" # Model bert đã dc fine-tune
 tokenizer = AutoTokenizer.from_pretrained(modelname, token=hf_token)
-mbert = AutoModel.from_pretrained(modelname,  token=hf_token).to(device)
+mbert = AutoModel.from_pretrained(modelname, token=hf_token).to(device)
 model = MBERTClassifier(mbert, num_classes=3).to(device)
-#model.load_state_dict(torch.load('Model/checkpoint.pt', map_location=device))
+model.load_state_dict(torch.load('Model/checkpoint.pt', map_location=device))
 
 # Hàm để dự đoán nhãn
 def predict(context, claim):
 
-#    gc.collect()
-#    torch.cuda.empty_cache()
-#    torch.cuda.ipc_collect()
+    # gc.collect()
+    # torch.cuda.empty_cache()
+    # torch.cuda.ipc_collect()
 
     # evidence_top5, top5_consine = evidence_top_n(context, claim)
     # evidence_top1, top1_consine, rank_5 = similarities(evidence_top5, claim, top5_consine)
@@ -62,7 +78,6 @@ def predict(context, claim):
 # verdict, probabilities = predict(context, claim)
 
 # print(f"Verdict: {verdict}")
-
 
 # # Display percentages with colors
 # labels = ["SUPPORTED", "REFUTED", "NEI"]
